101d196de7bcf7a5f82ac9eb780097b7cf771a08
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-07-10 16:15:30 +0700

    yo

--- object/tugas.js
+++ object/tugas.js
@@ -30,6 +30,34 @@ const grouping = scores => {
 
 // console.table(grouping([80, 45, 90, 65, 74, 100, 85, 30]));
 
+//1 bagian 2
+
+const grouping2 = scores => {
+    let result = {
+        phase1: [],
+        phase2: [],
+        phase3: [],
+        phase4: []
+    }
+    scores.forEach(score => {
+        if(score <= 30){
+            result.phase1.push(score)
+        } else if(score > 30 &&  score <=  60){
+            result.phase2.push(score)
+        } else if(score > 60 &&  score <= 80){
+            result.phase3.push(score)
+        } else if(score > 80 &&  score <= 100) {
+            result.phase4.push(score)
+        } else {
+            console.log('error')
+        }
+    })
+    return result
+}
+// console.log(grouping2([10, 20, 25, 30, 60, 55, 35, 31, 70, 75, 65, 61, 81, 55, 35, 31]));
+
+
+
 //2
 
 let arr = [
@@ -51,7 +79,11 @@ const convert = data => {
     })
     return result
 }
-// console.table(convert(arr))
+// console.table(convert([
+//     [1, 'Ridho Majid', 'Back End Developer', 'Jakarta'],
+//     [2, 'David Winalda', 'Front End Developer', 'Bandung'],
+//     [3, 'Miftah faris', 'Mentor', 'Jakarta'],
+//   ]))
 
 //3 
 const csvToObject = text => {
@@ -65,4 +97,30 @@ const csvToObject = text => {
     return result
 }
 
-console.log(csvToObject('David Winalda, Impact Byte, Male'))
+// console.table(csvToObject('David Winalda, Impact Byte, Male'));
+// console.table(csvToObject('Ridho Majid, Impact Byte, Male'));
+
+// 4
+// looping for in
+
+// const deleteUndefinedKeys = (obj) => {
+//  for(let mantap in obj){
+//      if(obj[mantap] === undefined){
+//          delete obj[mantap]
+//      }
+//  }
+//  return obj
+// }
+
+// console.log(deleteUndefinedKeys({name: 'agungmubarok', age: undefined, email: 'agungmubarok@gmail.com'}))
+
+let siswa = {
+    nama: 'ridho',
+    alamat: 'Citayam',
+    usia: 26
+}
+
+for (let key in siswa){
+    console.log(`key = ${key}, value= ${siswa[key]}`);  
+}
+
