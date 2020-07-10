85b39bc744092d7bc8f52db327a87d6ee3733216
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-07-10 11:31:52 +0700

    ,

--- object/tugas.js
+++ object/tugas.js
@@ -10,4 +10,59 @@ let obj = {
     }
 }
 
-console.log(obj.greeting());
+// console.log(obj.greeting());
+
+//1
+const grouping = scores => {
+    let result = {
+        pass: [],
+        fail: []
+    }
+    scores.forEach(element => {
+        if(element < 75){
+            result.fail.push(element)
+        } else {
+            result.pass.push(element)
+        }
+    })
+    return result
+}
+
+// console.table(grouping([80, 45, 90, 65, 74, 100, 85, 30]));
+
+//2
+
+let arr = [
+            [1, 'Ridho Majid', 'Back End Developer', 'Jakarta'],
+            [2, 'David Winalda', 'Front End Developer', 'Bandung'],
+          ]
+
+const convert = data => {
+    let result = []
+
+    data.map(el => {
+            const obj = {
+            id: el[0],
+            name: el[1],
+            work: el[2],
+            location: el[3]
+        }
+        result.push(obj)
+    })
+    return result
+}
+// console.table(convert(arr))
+
+//3 
+const csvToObject = text => {
+    let result = {}
+    let a =text.split(',')
+
+    result.name = a[0];
+    result.bootcamp = a[1];
+    result.gender = a[2]
+    
+    return result
+}
+
+console.log(csvToObject('David Winalda, Impact Byte, Male'))
