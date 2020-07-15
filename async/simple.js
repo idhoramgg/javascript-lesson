a746ccfda63fe5708f217f32fad607175938d416
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-07-15 12:14:16 +0700

    add contoh onclick by id, get data

--- async/simple.js
+++ async/simple.js
@@ -92,7 +92,7 @@ const contohPromise = () => {
     }).then(result => console.log(result))
       .catch(error => console.log(error))
 }
-contohPromise()
+// contohPromise()
 
 // contoh Promise req API dengan fetch()
     // fetch() -> function bawaan javascript untuk mengambil/mengolah data API
@@ -167,7 +167,7 @@ const getDataAPIwithAsync = async () => {
 
     let response = await fetch(API, option)
     response = await response.json()
-    console.log(response);
+    // console.log(response);
 }
 getDataAPIwithAsync()
 
@@ -181,26 +181,138 @@ getDataAPIwithAsync()
 
 
 
+// const dataArray = [12,32,123,32,124,52]
 
 
 
+// async function
+
+const p1 = () => {
+    setTimeout(() => {
+        console.log('p1 selesai');
+    }, 4000)
+}
+
+const p2 = () => {
+  setTimeout(()=>{
+      console.log('p2 selesai');
+  },3000)
+}
+
+const p3 = () => {
+  p1()
+  p2()
+  console.log('p3 selesai');
+}
 
 
 
+// p3()
+
+// contoh sederhana promise
+const promiseFunc = () => {
+  new Promise((resolve, reject) => {
+      let condition = false;
+      if(condition){
+          resolve('fulfilled')
+      } else {
+          reject(new Error('error, rejected'))
+      }
+  }).then(result => console.log(result))
+    .catch(error => console.log(error))
+}
+
+// promiseFunc()
+
+// penulisan function async await
+// func biasa
+function getData(){
+    return 'Hello world'
+}
+
+const getData2 = () => {
+  return 'Hello world'
+}
+// func async await
+async function getData(){
+    let result = await fetch(api)
+    return result
+}
+// es6
+const getData23 = async () => {
+  let result = await "Hello world"
+  return result
+}
+
+/////////////////////////////////////////////
 
 
+// fetch()
+// promise based
+// const fetchData = () => {
+//     const API = "https://5e92be81bbff810016969173.mockapi.io/api/v1/users"
+//     const options = {
+//         method: "GET"
+//     }
+//   fetch(API, options)
+//   .then(result => result.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+// }
+// fetchData()
+// async await
+const fetchData2 = async () => {
+    const API = "https://5e92be81bbff810016969173.mockapi.io/api/v1/users"
+    const options = {
+        method: "GET"
+    }
+    try {
+        let text = ""
+        let response = await fetch(API, options)
+        let result = await response.json()
+        
+        result.map(data => {
+            let card = document.createElement('div');
+            card.classList.add('card')
+            // console.log(card);
+            text += `
+                    <button onclick="checkOne(${data.id})"class="btn-card" type="button"> 
+                    <div id="card" class="card">
+                     Full Name : ${data.fullName} <br>
+                     Phone Number : ${data.phoneNumber} <br>
+                     Email: ${data.email} <br>
+                     </div>
+                     </button>
+                    `
+                })
+                document.getElementById('container').innerHTML = text
 
+    } catch(error){
+        console.log(error)
+    }
+ }
+fetchData2()
 
+cardElement = document.querySelector('.btn-card')
 
-
-
-
-
-
-
-
-
-
-
-
+const checkOne = async (id) => {
+    const API = `https://5e92be81bbff810016969173.mockapi.io/api/v1/users/${id}`
+    const options = {
+        method: "GET"
+    }
+    const res = await fetch(API, options)
+    const result = await res.json()
+    alert(`
+        ID: ${result.id}
+        Full name: ${result.fullName}
+        Email: ${result.email}
+        Passowrd: ${result.password}
+        CreatedAt: ${result.createdAt}
+    `)
+}
+cardElement.addEventListener('click', checkOne)
+// error handling async await ada 3 cara
+// 1. if else condition
+// 2. try catch
+// 3. jangan buat error
 
